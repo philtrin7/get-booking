bc1beb14896c4eff95dd3de58434b0f622e8c748
philtrin7 <philtrin7@gmail.com>
2021-09-01 16:50:21 +1000

    feat(customer): new Booking model

--- getbooking/customer/models.py
+++ getbooking/customer/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -10,3 +11,14 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.user.get_full_name()
+
+
+class Booking(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    appointment_datetime = models.DateTimeField(blank=False)
+    reason = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.id
