a0979aa75a3be01894d91c421d77ff55412ad095
philtrin7 <philtrin7@gmail.com>
2021-09-01 16:22:25 +1000

    feat(customer): add Customer model

--- getbooking/customer/models.py
+++ getbooking/customer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255, blank=False)
+    phone_number = models.CharField(max_length=10, blank=False)
+    date_of_birth = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.get_full_name()
