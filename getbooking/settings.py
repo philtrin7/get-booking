4aab5997a49bd28ca67b28352ff2585f706e3bf1
philtrin7 <philtrin7@gmail.com>
2021-09-01 13:16:19 +1000

    refactor(templates): move core and customer templates up to project-level

--- getbooking/settings.py
+++ getbooking/settings.py
@@ -46,10 +46,7 @@ ROOT_URLCONF = 'getbooking.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'getbooking', 'core', 'templates'),
-            os.path.join(BASE_DIR, 'getbooking', 'customer', 'templates'),
-        ],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
