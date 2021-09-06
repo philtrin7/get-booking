3e34e77ae5c8ac4d2c716f6be7228878d986803a
philtrin7 <philtrin7@gmail.com>
2021-09-06 14:23:13 +1000

    style(booking_form): basic bootstrap form styling

--- getbooking/customer/views.py
+++ getbooking/customer/views.py
@@ -6,4 +6,6 @@ from getbooking.customer import forms
 def appointment(request):
     booking_form = forms.CreateBookingForm()
 
-    return render(request, 'customer/appointment.html')
+    return render(request, 'customer/appointment.html', {
+        "booking_form": booking_form
+    })
