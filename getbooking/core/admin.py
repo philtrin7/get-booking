141bf4b2c838efa144f9003723e23ddc0bc124f3
philtrin7 <philtrin7@gmail.com>
2021-09-14 14:33:25 +1000

    feat(customer): create user and customer withffirst time mobile sign in

--- getbooking/core/admin.py
+++ getbooking/core/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from getbooking.customer import models
+from getbooking.customer.models import Customer
 
 
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['phone_number', ]
 
 
-admin.site.register(models.Customer)
+admin.site.register(Customer, CustomerAdmin)
--- getbooking/customer/views.py
+++ getbooking/customer/views.py
@@ -3,6 +3,8 @@ from firebase_admin import credentials, auth
 from django.shortcuts import render
 from django.conf import settings
 
+from django.contrib.auth.models import User
+from getbooking.customer.models import Customer
 from getbooking.customer import forms
 
 cred = credentials.Certificate(settings.FIREBASE_ADMIN_CREDENTIALS)
@@ -23,6 +25,15 @@ def verify_mobile(request):
             try:
                 firebase_user = auth.verify_id_token(
                     request.POST.get('id_token'))
+                phone_number = firebase_user['phone_number']
+
+                if not User.objects.filter(username=phone_number):
+                    user = User.objects.create_user(username=phone_number)
+                    user.set_unusable_password()
+                    user.save()
+
+                    Customer.objects.create(
+                        user=user, phone_number=phone_number)
 
             except Exception as e:
                 print(e)
