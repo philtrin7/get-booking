bc67910fc84621bf545abdfe65e83f8ce1d197b5
philtrin7 <philtrin7@gmail.com>
2021-09-28 16:30:45 +1000

    api(cronofy): api request for access_token

--- getbooking/provider/views.py
+++ getbooking/provider/views.py
@@ -1,4 +1,24 @@
+import requests
 from django.shortcuts import render
+
+
+def request_cronofy_access_token():
+    client_id = ""
+    client_secret = ""
+    grant_type = ""
+    refresh_token = ""
+
+    base_url = "https://api.cronofy.com/oauth/token?client_id=%s&client_secret=%s&grant_type=%s&refresh_token=%s" % (
+        client_id, client_secret, grant_type, refresh_token)
+
+    try:
+        request = requests.post(base_url)
+    except requests.exceptions.ConnectionError as err:
+        print(err)
+    except requests.exceptions.RequestException as err:
+        print(err)
+
+    return request.json()
 
 
 def view_week(request, year, month, day):
